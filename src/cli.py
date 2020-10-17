ddd83e7ed63596929c8709c51428cf54f279cdbb
Vyacheslav Krachkov <v.krachkov@enestech.net>
2020-10-17 22:16:52 +0300

    chg: reformat code

--- src/cli.py
+++ src/cli.py
@@ -11,10 +11,15 @@ def runserver(host: str = "localhost", port: int = 8000, debug: bool = True):
         typer.echo("Cannot star server, install uvicorn at first", err=True)
         raise typer.Exit(code=1)
 
-    from django_fastapi.main import application
+    from server import application
 
     typer.echo("Starting server ...")
     uvicorn.run(application, host=host, port=port, debug=debug)
+
+
+@application.command()
+def hello():
+    typer.echo('Hello')
 
 
 if __name__ == "__main__":
